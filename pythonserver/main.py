5e54a2209d60be859e9588a70559b2166b72aca7
aleksander <aleksander.hrastic@ltfe.org>
2022-04-25 17:40:02 +0200

    dela?

--- pythonserver/main.py
+++ pythonserver/main.py
@@ -1,5 +1,5 @@
 # python3.6
-
+from datetime import datetime
 
 from flask import Flask
 from paho.mqtt import client as mqtt_client
@@ -28,10 +28,20 @@ def connect_mqtt() -> mqtt_client:
     client.connect(broker, port)
     return client
 
-
+l = [] #list of values
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        #x = {
+        #    'value': msg,
+        #   'time': datetime.now().timestamp()
+        #}
+        x = msg
+        #save samples to array of length of 10 min ? 
+        l.append(x)   #add measurement to list                     
+        l = l[-6:]    #redefine list as last 5 items 
+
+
 
     client.subscribe(topic)
     client.on_message = on_message
@@ -43,12 +53,30 @@ def run():
     client.loop_start()
     app.run(host='0.0.0.0', port=8080)
 
-
-
+ measure = 0
+ values = []
+def collectingSamples(userID, time, device, startEnd):
+    #start collecting samples, gets called with api call, last minute and counting untill another api call, alghoritm for recognizing 
+    # max value and min 
+    console.log("measuring")
+    userConsumption = 0
+    if startEnd: #start =1, end = 0
+        measure = 1
+        values.extend(l)
+    else:
+        measure = 0
+    while measure:
+        values.append(l[-1])
+    min = min(values)
+    
+    userConsumption = 
+    return userConsumption
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! I am running on port ' + str(port)
+def measurement(data):
+    cons = collectingSamples(data['userID'], data['time'], data['device'], data['startEnd'])
+    return cons
+
 
 if __name__ == '__main__':
     run()
