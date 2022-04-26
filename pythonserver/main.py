8a68b65f40e218a9e03ccb6fcb73b721a2e1cc0b
Monika Bogataj <44950266+bmonika96@users.noreply.github.com>
2022-04-26 12:11:43 +0200

    Add files via upload

--- pythonserver/main.py
+++ pythonserver/main.py
@@ -1,7 +1,7 @@
 # python3.6
 from datetime import datetime
-
-from flask import Flask
+from threading import Event, Thread
+from flask import Flask, request
 from paho.mqtt import client as mqtt_client
 
 app = Flask(__name__)
@@ -29,17 +29,26 @@ def connect_mqtt() -> mqtt_client:
     return client
 
 l = [] #list of values
+start_measure = Event()
+values = []
+
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-        #x = {
-        #    'value': msg,
-        #   'time': datetime.now().timestamp()
-        #}
-        x = msg
-        #save samples to array of length of 10 min ? 
-        l.append(x)   #add measurement to list                     
-        l = l[-6:]    #redefine list as last 5 items 
+        global l, values
+        x = {
+            "value": int(msg.payload.decode()),
+            "time": datetime.now().timestamp()
+        }
+        #save samples to array of length of 10 min ?
+        l.append(x)   #add measurement to list
+        values.append(x)
+        if len(values) > 3:
+            values = values[-3:]
+        if start_measure.is_set():
+            values.append(l[-1])
+            print("adding to values")
+        l = l[-30:]    #redefine list as last 5 min
 
 
 
@@ -53,30 +62,57 @@ def run():
     client.loop_start()
     app.run(host='0.0.0.0', port=8080)
 
- measure = 0
- values = []
-def collectingSamples(userID, time, device, startEnd):
-    #start collecting samples, gets called with api call, last minute and counting untill another api call, alghoritm for recognizing 
-    # max value and min 
-    console.log("measuring")
-    userConsumption = 0
-    if startEnd: #start =1, end = 0
-        measure = 1
-        values.extend(l)
-    else:
-        measure = 0
-    while measure:
-        values.append(l[-1])
-    min = min(values)
-    
-    userConsumption = 
-    return userConsumption
+start_time = 0
+end_time = 0
 
-@app.route('/')
-def measurement(data):
-    cons = collectingSamples(data['userID'], data['time'], data['device'], data['startEnd'])
-    return cons
+def collectingSamples(userID, time, device, start):
+    #start collecting samples, gets called with api call, last minute and counting untill another api call, alghoritm for recognizing
+    # max value and min
+    print("collecting samples")
+    global start_time, end_time
+    userConsumption = 0
+    if start:
+        start_time = time
+        if not start_measure.is_set(): #start =1, end = 0
+            start_measure.set()
+        return "measuring"
+    else:
+        end_time=time
+        start_measure.clear()
+        print(values)
+        print(len(values))
+        sum = calculateConsumption(values,start_time, end_time)
+        return "calculated" + str(sum)
+
+def calculateConsumption(values, start_time, end_time):
+    sum_consumption = 0
+    measurements_list = [measure["value"] for measure in values]
+
+    min_measure = min(measurements_list)
+    print(min_measure)
+    for measurement1 in values:
+        if measurement1["time"] > start_time:
+            if measurement1["time"] < end_time:
+                print("is bigger than " + str(start_time))
+                sum_consumption += 1/360 * (measurement1["value"] - min_measure)
+    return sum_consumption
+
+
+
+@app.route('/',methods=['POST'])
+def measurement():
+    print("hello")
+    request_json = request.get_json()
+    # look for how to get data
+    user_id = request_json.get('userID')
+    start = request_json.get('start')
+    device = request_json.get('device')
+    time = request_json.get('time')
+    print(user_id, start, device, time)
+
+    return collectingSamples(user_id,time,device,start)
 
 
 if __name__ == '__main__':
     run()
+
